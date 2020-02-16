d5e0ef80a4dad9125fdffaabe1e3c064a19935dd
Wouter Verweirder <wouter.verweirder@gmail.com>
2020-02-16 17:47:46 +0100

    move playback to last demo in list, bugfix error on kinect close

--- examples/electron/renderer/demo-preload.js
+++ examples/electron/renderer/demo-preload.js
@@ -5,6 +5,9 @@ ipcRenderer.on('close-kinect', (event, message) => {
     console.log('close the kinect');
     window.kinect.stopListening().then(() => {
       console.log('kinect closed');
+    }).catch(e => {
+      console.error(e);
+    }).then(() => {
       window.kinect.destroyTracker();
       window.kinect.stopCameras();
       window.kinect.close();
