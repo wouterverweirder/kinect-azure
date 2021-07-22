a0bff7ac19b1675b91cfcab317455650a3b32edc
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-07-22 11:30:57 +0200

    fixes to electron demos to make them compatible with electron 9.x - closes #39

--- examples/electron/main.js
+++ examples/electron/main.js
@@ -13,7 +13,8 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      webviewTag: true
+      webviewTag: true,
+      contextIsolation: false
     }
   })
 
