7db779fa72db85141a41e4a5f67bee32562963c9
Min Oh <challenger71498@gmail.com>
2021-07-30 16:39:37 +0900

    Add linux body tracking sdk headers

---
+++ sdk/bt-sdk-1.1.0/build/linux/include/k4abtversion.h
@@ -0,0 +1,18 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+#ifndef K4ABTVERSION_H
+#define K4ABTVERSION_H
+
+#define K4ABT_VERSION_MAJOR           1
+#define K4ABT_VERSION_MINOR           1
+#define K4ABT_VERSION_PATCH           0
+#define K4ABT_VERSION_PRERELEASE      ""
+#define K4ABT_VERSION_BUILD_METADATA  ""
+
+#define K4ABT_VERSION_STR             "1.1.0"
+
+#define K4ABT_DNN_MODEL_FILE          "dnn_model_2_0_op11.onnx"
+#define K4ABT_DNN_MODEL_FILE_LITE     "dnn_model_2_0_lite_op11.onnx"
+
+#endif
